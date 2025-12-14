e9e76e184781ed404841d37fca081ec23a2716e4
cauphenuny <starseekist@gmail.com>
2025-12-14 12:58:56 +0800

    feat: multilevel log spec

--- src/mesh/meshark/apps/simplify.cc
+++ src/mesh/meshark/apps/simplify.cc
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <meshark/mesh-io.h>
 #include <meshark/mesh-simplifier.h>
+#include <spdlog/spdlog.h>
 using namespace meshark;
 
 int main(int argc, char** argv) {
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <input obj path> <output obj path> <ratio>"
+    if (argc != 4 && argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " <input obj path> <output obj path> <ratio> [-v]"
                   << std::endl;
         return 1;
+    }
+    if (argc == 5 && std::string(argv[4]) == "-v") {
+        spdlog::set_level(spdlog::level::debug);
+    } else {
+        spdlog::set_level(spdlog::level::info);
     }
     auto mesh = readGeometryMeshFromWavefrontObj(argv[1]);
     std::unique_ptr<MeshSimplifier> simplifier = std::make_unique<MeshSimplifier>(*mesh);
